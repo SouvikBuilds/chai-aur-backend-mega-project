2346d0dbd0aa4278abdccc33a7f1d5feb05170cf
Souvik Chatterjee <souvikchatterjee@Souviks-MacBook-Air.local>
2025-10-23 11:51:04 +0530

    Refactor logOutUser to use  for clearing refreshToken

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -155,7 +155,7 @@ const loginUser = asyncHandler(async (req, res, next) => {
 const logOutUser = asyncHandler(async (req, res, next) => {
   await User.findByIdAndUpdate(
     req.user._id,
-    { $set: { refreshToken: undefined } },
+    { $unset: { refreshToken: 1 } },
     { new: true }
   );
 
